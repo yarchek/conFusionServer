99b78e7402d88b6f374085642e9028f981dc7c36
yarchek <y.chekotun-2023@kpi.ua>
2019-05-19 18:41:19 +0300

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 
@@ -42,27 +44,21 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-  console.log(req.session);
 
-  if(!req.session.user) {
+  if(!req.user) {
     const err = new Error('You are not authenticated!');
     err.status = 403;
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      const err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
-  }
+    next();
 }
 
 app.use(auth);
